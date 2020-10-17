2f02684b1c707053a5b4282531b6affc3ef65e51
Alicja <ala.holowiecka@gmail.com>
2020-10-17 23:24:37 +0200

    zmiany w spisie treści w tex, próba wybrania krajów

--- covid_3009.R
+++ covid_3009.R
@@ -4,8 +4,16 @@ covid <- read.csv(url("https://raw.githubusercontent.com/owid/covid-19-data/mast
                   sep = ",", header = T)
 head(covid)
 
-covid %>% 
-  distinct(location)
+kraje <- covid %>% 
+  distinct(location) %>% 
+  as.data.frame()
+kraje_id <- c(1,2,3,4,5,8,9,11,12,13,15,16,18,19,20,21,23,24,26,27,28,31,32,34,
+              35,36,39,40,41,42,43,45,46,47,48,49,51,52,53,54,56,57,58,59,
+              63,64,68,69,71,72,73,74,75,77,81,89,90,91,92,93,94,95,97,98,100,
+              103,104,109,110,113,114,115,116,117,120,126,127,128,129, 130,
+              132,133,137,139,140,141,142,144,145,146,147,148,150,151,153,154,156,
+              157,158,159,165,166,167,170,172,173,175,176,177,178,179,180,181,183,
+              184,185,186,189,190,194,197,198,199,200,201,203,204,205,206,207,209,210,211)
 # 210 krajow + world + international 
 # pytanie czy brac pod uwage jakies male kraiki, wysepki itd
 
@@ -34,7 +42,9 @@ library(lattice)
 xyplot(total_cases_per_million~time|location,
        data = covid)
 
-library(nlme)
+library(nlme) #lmer4
+# population density, liczba testow, PKB, extreme poverty, odsetek osob z problemami sercowymi, life expectancy (przecietny czas zycia)
+
 mod <- lme(total_cases_per_million~time, 
            random = ~1|location,
            data = covid)
