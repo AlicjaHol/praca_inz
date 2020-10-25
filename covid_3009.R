a16c5b03610111d868ad98bed8b09b2d96b3433d
Alicja <ala.holowiecka@gmail.com>
2020-10-25 22:48:48 +0100

    nowe modele, usuwanie braków

--- covid_3009.R
+++ covid_3009.R
@@ -17,19 +17,23 @@ kraje_id <- c(1,2,3,4,5,8,9,11,12,13,15,16,18,19,20,21,23,24,26,27,28,31,32,34,
 # 210 krajow + world + international 
 # pytanie czy brac pod uwage jakies male kraiki, wysepki itd
 
+#covid <- covid[(covid$location!="International"& covid$location!="World"& covid$location!="Aruba"),]
+#covid <- drop_na(covid, location)
+covid <- covid[!covid$location %in% c("International", "World", "Kosovo", 
+                                      "South Sudan", "Syria", "Taiwan"),]
+covid <- covid[covid$population>1000000,]
 # co z total cases==NA, czy mozna zastapic wartoscia z poprzedniego dnia?
 
 # dla kazdego kraju, wykasowac dni zanim pojawil sie pierwszy przypadek
 
 # zamiast dat numery kolejnych dni trwania pandemii
-covid <- covid %>% 
-  select(location, date, total_cases, new_cases,
-         total_deaths, new_deaths, total_cases_per_million, new_cases_per_million,
-         new_tests, total_tests, total_tests_per_thousand, tests_per_case, stringency_index,
-         population, population_density, life_expectancy)
+#covid <- covid %>% 
+#  select(location, date, total_cases, new_cases,
+#         total_deaths, new_deaths, total_cases_per_million, new_cases_per_million,
+#         new_tests, total_tests, total_tests_per_thousand, tests_per_case, stringency_index,
+#         population, population_density, life_expectancy)
 
-covid <- covid %>% 
-  fill(total_cases_per_million)
+covid <- drop_na(covid, total_cases_per_million)
 
 
 
@@ -44,19 +48,103 @@ xyplot(total_cases_per_million~time|location,
 
 library(nlme) #lmer4
 # population density, liczba testow, PKB, extreme poverty, odsetek osob z problemami sercowymi, life expectancy (przecietny czas zycia)
-
+#library(lmer4)
 mod <- lme(total_cases_per_million~time, 
            random = ~1|location,
            data = covid)
+#mod <- lmer(total_cases_per_million~time+(1~location), data = covid)
 summary(mod)
 # oba wspolczynniki efektow stalych sa istotne (intercept i wsp przy time)
 # wyjaśniona wariancja przez efekt losowy 2943.93 - porównywalne z Residuals 2538.357
 AIC(mod) # miara utraconej informacji - im mniej tym lepiej
 
+aggregate(is.na(covid$total_tests_per_thousand), list(covid$location), FUN=sum)
 
-polska <- covid[covid$location=="Poland",]
 
-# dla krajow gdzie istnieja takie dane
-#mod1 <- lme(total_cases_per_million~time+total_tests_per_thousand,
-#           random = ~1|location,
-#           data = covid)
+
+
+covid_na <- drop_na(covid, total_tests_per_thousand)
+
+
+# TRZEBA BEDZIE NAJPIERW WYBRAĆ KRAJE GDZIE JEST MALO BRAKOW
+mod1 <- lme(total_cases_per_million~time+total_tests_per_thousand,
+           random = ~1|location,
+           data = covid_na)
+summary(mod1)
+
+
+covid$age <- 0
+covid[covid$life_expectancy<55,]$age <- "below 55"
+covid[covid$life_expectancy>=55&covid$life_expectancy<60,]$age <- "55-59"
+covid[covid$life_expectancy>=60&covid$life_expectancy<65,]$age <- "60-64"
+covid[covid$life_expectancy>=65&covid$life_expectancy<70,]$age <- "65-69"
+covid[covid$life_expectancy>=70&covid$life_expectancy<75,]$age <- "70-74"
+covid[covid$life_expectancy>=75&covid$life_expectancy<80,]$age <- "75-79"
+covid[covid$life_expectancy>=80,]$age<- "80 and above"
+covid$age <- as.factor(covid$age)
+
+
+
+
+
+
+
+mod2 <- lme(total_cases_per_million~time+age,
+            random=~1|location,
+            data=covid)
+summary(mod2)
+
+
+mod3 <- lme(total_cases_per_million~time+population_density,
+            random=~1|location,
+            data = covid)
+summary(mod3)
+
+
+covid_si <- drop_na(covid, stringency_index)
+mod4 <- lme(total_cases_per_million~time+stringency_index,
+            random=~1|location,
+            data = covid_si)
+summary(mod4)
+
+covid_hdi <- drop_na(covid, human_development_index)
+mod5 <- lme(total_cases_per_million~time+human_development_index,
+            random=~1|location,
+            covid_hdi)
+summary(mod5)
+
+
+mod6 <- lme(total_cases_per_million~time+cardiovasc_death_rate,
+            random=~1|location,
+            data= covid)
+summary(mod6)
+
+mod7 <- lme(total_cases_per_million~time+diabetes_prevalence,
+            random=~1|location,
+            data= covid)
+summary(mod7)
+
+
+covid_ep <- drop_na(covid, extreme_poverty)
+mod8 <- lme(total_cases_per_million~time+extreme_poverty,
+            random=~1|location,
+            data= covid_ep)
+summary(mod8)
+
+
+covid_gdp <- drop_na(covid, gdp_per_capita)
+mod9 <- lme(total_cases_per_million~time+gdp_per_capita,
+            random=~1|location,
+            data= covid_gdp)
+summary(mod9)
+
+
+mod10 <- lme(total_cases_per_million~time+age+stringency_index,
+             random=~1|location,
+             data=covid_si)
+summary(mod10)
+
+mod11 <- lme(total_tests_per_thousand~time+total_cases_per_million,
+             random=~1|location,
+             data=covid_na)
+summary(mod11)
